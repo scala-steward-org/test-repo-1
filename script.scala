237d4883eb3ef5c6409e4b09dbca067b0eaeffda
Frank Thomas <f.thomas@gmx.de>
2023-02-11 21:18:39 +0100

    Update script.scala

--- script.scala
+++ script.scala
@@ -2,6 +2,7 @@
 //> using lib "com.lihaoyi::os-lib:0.7.8"
 
 //> using lib "org.typelevel::cats-core:2.9.0"
+//> using lib "io.circe::circe-fs2:0.14.0"
 
 object Hello extends App {
   println("Hello World")
