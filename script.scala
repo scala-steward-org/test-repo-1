81c2c7159517f9fd4043c799bd8a659ec194f79d
Scala Steward <me@scala-steward.org>
2023-03-11 10:42:36 +0100

    Update os-lib to 0.9.1 in scala-cli-test

--- script.scala
+++ script.scala
@@ -1,5 +1,5 @@
 //> using scala "2.13.10"
-//> using lib "com.lihaoyi::os-lib:0.7.8"
+//> using lib "com.lihaoyi::os-lib:0.9.1"
 
 //> using lib "org.typelevel::cats-core:2.9.0"
 //> using lib "io.circe::circe-fs2:0.14.0"
