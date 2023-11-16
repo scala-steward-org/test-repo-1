cf82d7a31fac275ae5756c6f4270452991940d0b
Scala Steward <me@scala-steward.org>
2023-11-16 20:55:46 +0100

    Update cats-core to 2.10.0 in scala-cli-test

--- script.scala
+++ script.scala
@@ -1,7 +1,7 @@
 //> using scala "2.13.11"
 //> using lib "com.lihaoyi::os-lib:0.9.1"
 
-//> using lib "org.typelevel::cats-core:2.9.0"
+//> using lib "org.typelevel::cats-core:2.10.0"
 //> using lib "io.circe::circe-fs2:0.14.1"
 
 object Hello extends App {
