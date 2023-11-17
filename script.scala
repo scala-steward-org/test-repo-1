7a4150ed932eb6a05fe1415b6b57bd766235beb5
Scala Steward <me@scala-steward.org>
2023-11-17 07:37:53 +0100

    Revert commit(s) c9b82f7

--- script.scala
+++ script.scala
@@ -1,5 +1,5 @@
 //> using scala "2.13.11"
-//> using lib "com.lihaoyi::os-lib:0.9.2"
+//> using lib "com.lihaoyi::os-lib:0.9.1"
 
 //> using lib "org.typelevel::cats-core:2.9.0"
 //> using lib "io.circe::circe-fs2:0.14.1"
