7ca1ff34dd428ba75f5a5bac80c0d5e55acade63
Frank Thomas <f.thomas@gmx.de>
2024-09-11 09:00:56 +0200

    Update script.scala

--- script.scala
+++ script.scala
@@ -1,6 +1,7 @@
 //> using scala "2.13.14"
 //> using lib "com.lihaoyi::os-lib:0.9.3"
 
+//> using lib "com.lihaoyi::fansi:0.3.1"
 //> using lib "org.typelevel::cats-core:2.10.0"
 //> using lib "io.circe::circe-fs2:0.14.1"
 //> using toolkit typelevel:0.1.21
