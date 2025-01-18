11752053139ce5360f2d93f8b4f4136102a3fd58
Scala Steward <dev@scala-steward.org>
2025-01-18 12:06:30 +0100

    Update os-lib to 0.11.3 in scala-cli-test

--- script.scala
+++ script.scala
@@ -1,5 +1,5 @@
 //> using scala "2.13.16"
-//> using lib "com.lihaoyi::os-lib:0.9.3"
+//> using lib "com.lihaoyi::os-lib:0.11.3"
 
 //> using lib "com.lihaoyi::fansi:0.3.1"
 //> using lib "org.typelevel::cats-core:2.12.0"
