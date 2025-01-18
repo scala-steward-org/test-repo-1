b79f294906e72f2e9d8ae808d1853abeee549637
Scala Steward <dev@scala-steward.org>
2025-01-18 12:06:16 +0100

    Update fansi to 0.5.0 in scala-cli-test

--- script.scala
+++ script.scala
@@ -1,7 +1,7 @@
 //> using scala "2.13.16"
 //> using lib "com.lihaoyi::os-lib:0.9.3"
 
-//> using lib "com.lihaoyi::fansi:0.3.1"
+//> using lib "com.lihaoyi::fansi:0.5.0"
 //> using lib "org.typelevel::cats-core:2.12.0"
 //> using lib "io.circe::circe-fs2:0.14.1"
 //> using toolkit typelevel:0.1.29
