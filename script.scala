ff1e2e30de96a64b459e12a3a05254b64cfcc541
Scala Steward <dev@scala-steward.org>
2025-01-24 15:46:46 +0100

    Update log4cats-slf4j to 1.7.0 in scala-cli-test

--- script.scala
+++ script.scala
@@ -4,7 +4,7 @@
 //> using dep "com.lihaoyi::fansi:0.5.0"
 //> using dep "org.typelevel::cats-core:2.12.0"
 //> using dep "io.circe::circe-fs2:0.14.1"
-//> using dep "io.chrisdavenport::log4cats-slf4j:1.1.1"
+//> using dep "org.typelevel::log4cats-slf4j:1.7.0"
 //> using toolkit typelevel:0.1.29
 
 import org.typelevel.log4cats.Logger
