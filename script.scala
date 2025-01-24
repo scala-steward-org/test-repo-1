2d44a962741651e7ae8a20327724f7dec32888d1
Scala Steward <dev@scala-steward.org>
2025-01-24 15:46:46 +0100

    Applied Scalafix rule(s) https://gist.githubusercontent.com/fthomas/ea14d58e1b445d8038a5bc22acf751e8/raw/e1180c91dab0b36d8f9ec37d413ef9507bee67ff/log4cats-1.2.0.scala
    Co-authored-by: Frank Thomas <frank@timepit.eu>

--- script.scala
+++ script.scala
@@ -7,8 +7,8 @@
 //> using dep "io.chrisdavenport::log4cats-slf4j:1.1.1"
 //> using toolkit typelevel:0.1.29
 
-import io.chrisdavenport.log4cats.Logger
-import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object Hello extends App {
   println("Hello World")
