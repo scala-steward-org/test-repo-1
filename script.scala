936de54b98ed6eea127fb27b2ba279f39f45e721
Frank Thomas <frank@timepit.eu>
2025-01-24 15:32:02 +0100

    Use new syntax

--- script.scala
+++ script.scala
@@ -1,9 +1,9 @@
 //> using scala "2.13.16"
-//> using lib "com.lihaoyi::os-lib:0.11.3"
+//> using dep "com.lihaoyi::os-lib:0.11.3"
 
-//> using lib "com.lihaoyi::fansi:0.5.0"
-//> using lib "org.typelevel::cats-core:2.12.0"
-//> using lib "io.circe::circe-fs2:0.14.1"
+//> using dep "com.lihaoyi::fansi:0.5.0"
+//> using dep "org.typelevel::cats-core:2.12.0"
+//> using dep "io.circe::circe-fs2:0.14.1"
 //> using toolkit typelevel:0.1.29
 
 object Hello extends App {
