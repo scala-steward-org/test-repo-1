31d7d1961d4cee0990a3a653b2f144ec828dc66c
Frank Thomas <frank@timepit.eu>
2025-01-24 15:34:36 +0100

    Add old log4cats

--- script.scala
+++ script.scala
@@ -4,7 +4,11 @@
 //> using dep "com.lihaoyi::fansi:0.5.0"
 //> using dep "org.typelevel::cats-core:2.12.0"
 //> using dep "io.circe::circe-fs2:0.14.1"
+//> using dep "io.chrisdavenport::log4cats-slf4j:1.1.1"
 //> using toolkit typelevel:0.1.29
+
+import io.chrisdavenport.log4cats.Logger
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 
 object Hello extends App {
   println("Hello World")
