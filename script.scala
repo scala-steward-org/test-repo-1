658388381b49965ff05ee39be3caaf9890fe0b02
Scala Steward <dev@scala-steward.org>
2025-01-24 15:46:58 +0100

    Update cats-core to 2.13.0 in scala-cli-test

--- script.scala
+++ script.scala
@@ -2,7 +2,7 @@
 //> using dep "com.lihaoyi::os-lib:0.11.3"
 
 //> using dep "com.lihaoyi::fansi:0.5.0"
-//> using dep "org.typelevel::cats-core:2.12.0"
+//> using dep "org.typelevel::cats-core:2.13.0"
 //> using dep "io.circe::circe-fs2:0.14.1"
 //> using dep "io.chrisdavenport::log4cats-slf4j:1.1.1"
 //> using toolkit typelevel:0.1.29
